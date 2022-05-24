c2c17d9edd2aa5c7f4d47c241369f6ea5ba5998a
RodraxTB <rotabuendia3@gmail.com>
2022-05-24 12:42:16 +0200

    Update Partida.java

--- Partida.java
+++ Partida.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Partida {
+
     
+    private ArrayList<Integer> bombo = new ArrayList<Integer>(90);
+  
+    public Partida(){
+
+        //números bombo
+        for (int i = 1; i < bombo.size(); i++) {  
+            bombo.add(i);
+        }
+
+
+    }
 }
 /**/
