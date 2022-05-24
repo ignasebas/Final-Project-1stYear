443c4a0e984cf23ea5985cd67faf803436c7d5af
RodraxTB <rotabuendia3@gmail.com>
2022-05-24 12:27:20 +0200

    Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -5,22 +5,33 @@ public class Jugador {
     private String nombreJugador;
     private ArrayList<Carton> cartones;
 
-    public Jugador(String nombre, int cantidadCartones){
+    /*crear dependiendo de la cantidad de cartones creare una arraylist de 15 de largo y luego creo 15 cartones random
+      y los introduzco en la arraylist*/
+    public Jugador(String nombre, int cantidadCartones){  
 
         this.nombreJugador = nombre;
         this.cartones = new ArrayList<Carton>(cantidadCartones);
 
+        for (int i = 0; i < cantidadCartones; i++) {
+
+            Carton nuevoCarton = new Carton();
+            cartones.add(nuevoCarton);
+            
+        }
+
     }
 
-    
-    
-    public int cartonJugador(){
+    private void tacharNumeros(int numero){
 
+        System.out.println("El jugador "+nombreJugador+" ha tachado el número "+numero);
 
+        for (Carton carton : cartones) {
 
-    }
-
-    private void tacharNumeros(){
+            if(carton.tacharNumero(numero)){
+                System.out.println(carton);
+            }
+            
+        }
 
     }
 
