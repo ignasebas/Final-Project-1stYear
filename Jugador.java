3c7850480ce36fe11a0f9b7b5f85dd84c6d60482
RodraxTB <rotabuendia3@gmail.com>
2022-05-24 10:47:39 +0200

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Jugador {
+
+    private String nombreJugador;
+    private ArrayList<Carton> cartones;
+
+    public Jugador(String nombre, int cantidadCartones){
+
+        this.nombreJugador = nombre;
+        this.cartones = new ArrayList<Carton>(cantidadCartones);
+
+    }
+
     
+    
+    public int cartonJugador(){
+
+
+
+    }
+
+    private void tacharNumeros(){
+
+    }
+
 }
